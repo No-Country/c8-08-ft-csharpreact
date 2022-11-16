56f1c9c09a841ffbb207537a4c3c1b1328ed9486
DeniseCardozo <dennicardozo96@gmail.com>
2022-11-16 14:02:23 -0300

    Arreglos y agredado de import React

--- Front-end/components/Cards/Cards.jsx
+++ Front-end/components/Cards/Cards.jsx
@@ -1,16 +1,15 @@
+import React from "react"
 import styles from "../Cards/styles/Cards.module.css"
 import Card from "../Cards/Card.jsx"
 
-export default function Cards() {
+export default function Cards () {
+  const numeritos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-    let numeritos = [1,2,3,4,5,6,7,8,9,10,11,12];
-    
-    return (
+  return (
         <div className={styles.container}>
             {numeritos && numeritos.map(
-                num => <Card num={num} key={num.toString()} />
+              num => <Card num={num} key={num.toString()} />
             )}
-
         </div>
-    )
+  )
 }
