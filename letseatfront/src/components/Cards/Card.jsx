37da4ee5c7944d7183de0ba52cf04d713190b67c
DeniseCardozo <dennicardozo96@gmail.com>
2022-12-01 14:33:44 -0300

    Agregar get de todos los restaurants y renderizado

--- letseatfront/src/components/Cards/Card.jsx
+++ letseatfront/src/components/Cards/Card.jsx
@@ -1,25 +1,43 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import styles from "./styles/Card.module.css"
 import { AiFillStar } from 'react-icons/ai'
 // import { TfiHeart } from 'react-icons/tfi'
 import { Link } from "react-router-dom";
 
-export default function Card ({ num }) {
+export default function Card ({ restaurant }) {
+  useEffect(() => {
+    return async () => {
+      console.log(restaurant)
+  }
+}, [restaurant]);
+  
+  let allScore = []
+
+  restaurant.scoreBusinesses.map((el) => { 
+    return (
+      allScore.push(el.score)
+    )
+  })
+
+
+
   return (
-    <Link to={`/detail/${num}`}>
+    <Link to={`/detail/${restaurant.id}`}>
     <div className={styles.container}>
-        <div className={styles.picture}>
+        <div className={styles.picture} 
+            style={{background: `url(${restaurant.pictureBusinesses[0].urlImg})`, backgroundSize: "cover"
+        }}>
             {/* <TfiHeart className={styles.like} /> */}
         </div>
         <div className={styles.boxTitle}>
-            <h3 className={styles.name}>Restaurante numero {num}!</h3>
+            <h3 className={styles.name}>{restaurant.name}</h3>
             <div className={styles.boxScore}>
                 <AiFillStar/>
                 <h5 className={styles.details}>5.0</h5>
             </div>
         </div>
-        <h5 className={styles.details}>Aqui irá algo de detalle sobre el restaurante</h5>
+        <h5 className={styles.details}>{restaurant.description}</h5>
     </div>
     </Link>
   )
