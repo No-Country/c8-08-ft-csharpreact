9bd0cd761ca7fe9f59c0a16ed8ff927f9db9b7dc
DeniseCardozo <dennicardozo96@gmail.com>
2022-12-06 11:45:05 -0300

    Incorporar POST de nuevo restaurante, modificaciones de vistas y funcionamiento, aplicado de styles

--- letseatfront/src/components/Restaurants/NewRestaurant.jsx
+++ letseatfront/src/components/Restaurants/NewRestaurant.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import styles from './styles/NewRestaurant.module.css'
-// import { IoIosCloseCircleOutline } from 'react-icons/io'
-// import { useDispatch } from 'react-redux'
+import axios from "axios"
+import Swal from "sweetalert2"
 
-export default function NewRestaurant ({setShowFormNewRestaurant, showFormNewRestaurant }) {
-    // const dispatch = useDispatch()
+const UrlNewRestaurant = "http://platano-001-site1.ftempurl.com/api/Business"
+
+export default function NewRestaurant ({setShowFormNewRestaurant, showFormNewRestaurant, OwnerId }) {
 
     function handleClick () {
         setShowFormNewRestaurant(!showFormNewRestaurant)
@@ -18,12 +19,16 @@ export default function NewRestaurant ({setShowFormNewRestaurant, showFormNewRes
         Adress: "",
         // Img: [],
       })
+
+      var formData = new FormData(); 
+
+      const [img,setImg]= useState(null)
     
       const handleInputChange = (e) => {
         e.preventDefault()
-        // if (e.target.type === "file") {
-        //   setInput({ ...input, [e.target.name]: e.target.files[0] })
-        // }
+        if (e.target.type === "file") {
+          setImg(e.target.files[0])
+        }
         setInput({
           ...input,
           [e.target.name]: e.target.value
@@ -31,56 +36,65 @@ export default function NewRestaurant ({setShowFormNewRestaurant, showFormNewRes
       }
     
       const handleSubmit = async (e) => {
+        formData.append('Img',img)
+        formData.append('Name',e.target.Name.value)
+        formData.append('Description',e.target.Description.value)
+        formData.append('Dpart',e.target.Dpart.value)
+        formData.append('Mund',e.target.Mund.value)
+        formData.append('Adress',e.target.Adress.value)
+        formData.append('SellerId', OwnerId)
+        // for (let [key, value] of formData) {
+        //   console.log(`${key}: ${value}`)
+        // }
         e.preventDefault()
-        // await axios.post(UrlUser, input, {
-        //   headers: {
-        //     "Content-Type": "multipart/form-data"
-        //   }
-        // })
-        // await axios({
-        //   method: 'post',
-        //   url: UrlUser,
-        //   data: formData,
-        //   header: {
-        //     // 'Accept': 'application/json',
-        //     'Content-Type': 'multipart/form-data'
-        //   }
-        // })
+        await axios.post(UrlNewRestaurant, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        })
           // .then(function (response) { console.log(response) })
-          // .catch(function (response) { console.log(response) })
+          .catch(function (response) { console.log(response) })
         setInput({
           Name: "",
           Description: "",
           Dpart: "",
           Mund: "",
           Adress: "",
+          SellerId: 0
           // Img: [],
-        })
-        // console.log(input)
-      }
+          })
+        setShowFormNewRestaurant(!showFormNewRestaurant)
+        Swal.fire({
+          title:'Buen trabajo!',
+          text:'Nuevo restaurante creado exitosamente!',
+          confirmButtonColor: "#3c8c6c",}
+        )
 
+      }
+    
+    
   return (
     <div className={ styles.container }>
         <div className={ styles.boxForm }>
        {/* <IoIosCloseCircleOutline className={styles.iconClose} onClick={handleClick} /> */}
 
-        <h1 className={styles.mainTitle}>You're almost done!</h1>
-                <h3 className={styles.secondaryTitle}>Add a new restaurant</h3>
+        <h1 className={styles.mainTitle}>Crear nuevo restaurante</h1>
+                <h3 className={styles.secondaryTitle}>Coloca toda la información sobre tu negocio</h3>
                 <form className={ styles.form } onSubmit={(e) => handleSubmit(e)}>
                     <div className={styles.secondaryBox}>
-                        <input className={styles.input} type="text" name="Name" value={input.Name} placeholder="Name" onChange={(e) => handleInputChange(e)}/>
+                        <input className={styles.input} type="text" name="Name" value={input.Name} placeholder="Nombre" onChange={(e) => handleInputChange(e)}/>
                         <input className={styles.input} accept="image/*" type="file" name="urlPhoto" value={input.urlPhoto} onChange={(e) => handleInputChange(e)}/>
                     </div>
                     <div className={styles.secondaryBox}>
-                        <input className={styles.input} type="text" name="Adress" value={input.Adress} placeholder="Address" onChange={(e) => handleInputChange(e)}/>
-                        <input className={styles.input} type="text" name="Mund" value={input.Mund} placeholder="Municipality" onChange={(e) => handleInputChange(e)}/>
-                        <input className={styles.input} type="text" name="Dpart" value={input.Dpart} placeholder="Department" onChange={(e) => handleInputChange(e)}/>
+                        <input className={styles.input} type="text" name="Adress" value={input.Adress} placeholder="Dirección" onChange={(e) => handleInputChange(e)}/>
+                        <input className={styles.input} type="text" name="Mund" value={input.Mund} placeholder="Municipio" onChange={(e) => handleInputChange(e)}/>
+                        <input className={styles.input} type="text" name="Dpart" value={input.Dpart} placeholder="Departamento" onChange={(e) => handleInputChange(e)}/>
                     </div>
                     <div className={styles.secondaryBox}>
-                        <textarea className={styles.inputTextarea} type="text" name="Description" value={input.Description} placeholder="Description" onChange={(e) => handleInputChange(e)}/>
+                        <textarea className={styles.inputTextarea} type="text" name="Description" value={input.Description} placeholder="Descripción" onChange={(e) => handleInputChange(e)}/>
                     </div>
-                    <button className={styles.button}>CREATE NEW RESTAURANT</button>
-                    <button className={styles.buttonCancel} onClick={handleClick}>CANCEL</button>
+                    <button className={styles.button}>CREAR</button>
+                    <button className={styles.buttonCancel} onClick={handleClick}>CANCELAR</button>
                 </form>
 
         </div>
